2f0b5534140894d3ce92fcac5f9807cc3b69e9a1
Namiko01 <vitaliknam01@gmail.com>
2025-09-05 00:50:25 +0300

    feat: qqqq

--- src/04-conditions-n-loops-tasks.js
+++ src/04-conditions-n-loops-tasks.js
@@ -316,8 +316,21 @@ function isCreditCardNumber(ccn) {
  *   10000 ( 1+0+0+0+0 = 1 ) => 1
  *   165536 (1+6+5+5+3+6 = 26,  2+6 = 8) => 8
  */
-function getDigitalRoot(/* num */) {
-  throw new Error('Not implemented');
+function getDigitalRoot(num) {
+  const sum = num
+    .toString()
+    .split('')
+    .map((el) => +el)
+    .reduce((el, acc) => el + acc, 0);
+  if (sum > 9) {
+    const sum2 = sum
+      .toString()
+      .split('')
+      .map((el) => +el)
+      .reduce((el, acc) => el + acc, 0);
+    return sum2;
+  }
+  return sum;
 }
 
 /**
